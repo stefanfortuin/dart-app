3323c144b665dfc555fdf180343f069f8a760819
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-03-20 11:51:03 +0100

    toast when score is out of range

--- resources/js/components/ScoreInput.vue
+++ resources/js/components/ScoreInput.vue
@@ -6,25 +6,34 @@
 			:value="value"
 			min=0
 			max=180
-			placeholder="Score"
+			placeholder="Hoeveel gooide je?"
 			pattern="[0-9]*" 
 			inputmode="numeric"
 			@change="handleNumberInput($event.target)"
-			class="w-full bg-gray-200 text-gray-700 font-bold placeholder-opacity-60 shadow-md placeholder-gray-400 py-2 px-4 mt-2 rounded-lg text-3xl"
+			class="w-full bg-gray-200 text-gray-700 font-bold placeholder-opacity-60 shadow-md placeholder-gray-400 py-2 px-4 mt-2 rounded-lg text-2xl"
 		>
 	</div>
 
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
 	props: ['user', 'value'],
 	methods: {
+		...mapActions({
+			notify: 'toast/add',
+		}),
 
 		handleNumberInput(target){
 			let input_value = parseInt(target.value);
-			if(this.scoreIsOutOfRange(target.min, target.max, input_value))
+			if(this.scoreIsOutOfRange(target.min, target.max, input_value)){
+				this.notify({
+					title: 'Score moet tussen 0 en 180',
+					type: 'error',
+				})
 				return
+			}
 
 			this.$emit('handleTurn', target)
 		},
