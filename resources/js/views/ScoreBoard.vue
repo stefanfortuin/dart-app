986d06179acc6cfebf5b7d37407abce4d086bfd4
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-03-20 13:29:04 +0100

    remove unecessary classes

--- resources/js/views/ScoreBoard.vue
+++ resources/js/views/ScoreBoard.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="w-full h-full flex flex-col ">
 		<game-info />
-		<div class="transition-transform transform">
+		<div>
 			<user-card-score
 				v-for="user in users"
 				:key="user.id"
