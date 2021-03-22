bac8070e73a22a3e2a5bf2a15f8f89dddd5e392d
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-03-22 20:55:11 +0100

    show leg owner in sets and legs table

--- resources/js/components/Info/SetsAndLegsTable.vue
+++ resources/js/components/Info/SetsAndLegsTable.vue
@@ -1,20 +1,26 @@
 <template>
 	<div class="flex justify-between items-end">
-		<div class="table p-2 w-40 text-white">
+		<div class="table px-2 pb-2 pt-1 w-40 text-white">
 			<div class="table-row-group">
 				<div class="table-row">
 					<div class="table-cell"></div>
-					<div class="table-cell text-center">Sets</div>
-					<div class="table-cell text-center">Legs</div>
+					<div class="table-cell text-right">Sets</div>
+					<div class="table-cell text-right">Legs</div>
 				</div>
 				<div
 					v-for="user in users"
 					:key="user.id"
 					class="table-row"
 				>
-					<div class="table-cell">{{user.name}}</div>
-					<div class="table-cell text-center">{{user.sets_won}}</div>
-					<div class="table-cell text-center">{{user.legs_won}}</div>
+					<div class="table-cell">
+						{{user.name}}
+						<div
+							v-show="user == currentLegOwner"
+							class="inline-flex rounded-full h-2 w-2 bg-white"
+						></div>
+					</div>
+					<div class="table-cell text-right">{{user.sets_won}}</div>
+					<div class="table-cell text-right">{{user.legs_won}}</div>
 				</div>
 			</div>
 		</div>
@@ -26,7 +32,8 @@ import { mapState } from 'vuex'
 export default {
 	computed: {
 		...mapState({
-			users: state => state.users
+			users: state => state.users,
+			currentLegOwner: state => state.current_leg_owner
 		})
 	}
 }
