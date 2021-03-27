84b359669749e193f52cb57f30cc0f109ac7b2a0
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-03-27 16:11:41 +0100

    scoer input style change

--- resources/js/components/ScoreInput.vue
+++ resources/js/components/ScoreInput.vue
@@ -10,7 +10,7 @@
 			pattern="[0-9]*" 
 			inputmode="numeric"
 			@change="handleNumberInput($event.target)"
-			class="w-full bg-gray-200 text-gray-700 font-bold placeholder-opacity-60 shadow-md placeholder-gray-400 py-2 px-4 rounded-lg text-2xl"
+			class="w-full bg-white border-2 text-blue-500 font-semibold border-blue-200 py-2 px-4 placeholder-opacity-50 placeholder-gray-800 rounded-lg text-xl"
 		>
 	</div>
 
