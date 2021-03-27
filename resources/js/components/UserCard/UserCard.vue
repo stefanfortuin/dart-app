36a962ea9aaf8efad5f1ff10a38e48c7c115760c
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-03-27 09:23:12 +0100

    only transition colors for user card

--- resources/js/components/UserCard/UserCard.vue
+++ resources/js/components/UserCard/UserCard.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="transition-all first:rounded-t-lg last:rounded-b-lg px-3 py-2"
+		class="transition-colors first:rounded-t-lg last:rounded-b-lg px-3 py-2"
 		:class="user.is_on_turn ? 'bg-blue-500 text-white shadow-md' : 'bg-blue-100 text-blue-600 opacity-50 shadow'
 		"
 	>
