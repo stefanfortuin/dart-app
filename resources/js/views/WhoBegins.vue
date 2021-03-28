198431ccb475c2317d3b9b126ec9bfeff76c97ca
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-03-28 17:05:23 +0200

    who begins view change

--- resources/js/views/WhoBegins.vue
+++ resources/js/views/WhoBegins.vue
@@ -1,19 +1,18 @@
 <template>
 	<div class="flex flex-col w-full h-full font-semibold">
-		<h1 class="text-4xl self-start text-gray-700 font-bold">Wie begint?</h1>
-		<div class=" my-auto">
-			<div
-				v-for="user in users"
-				:key="user.name"
-				@click="handleWhoBegins(user)"
-				class="p-4 bg-blue-500 font-bold h-48 rounded-lg shadow-md text-white flex flex-col justify-between my-4"
-			>
-				<div class="text-5xl">
-					{{user.name}}
-				</div>
-				<div class="font-thin">
-					Laatste spel was <span class="font-semibold">3 dagen</span> geleden
-				</div>
+		<h1 class="text-5xl mb-4 self-start text-gray-700 font-bold"><span class="text-blue-500">Wie</span> begint?</h1>
+
+		<div
+			v-for="user in users"
+			:key="user.name"
+			@click="handleWhoBegins(user)"
+			class="p-4 bg-blue-500 font-bold h-48 rounded-lg shadow-md text-white flex flex-col justify-between my-2"
+		>
+			<div class="text-5xl">
+				{{user.name}}
+			</div>
+			<div class="font-thin">
+				Laatste spel was <span class="font-semibold">3 dagen</span> geleden
 			</div>
 		</div>
 	</div>
