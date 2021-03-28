9a6b08e389030c2463f3bc32095f447eca59ba85
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-03-28 16:42:29 +0200

    set and legs info layout

--- resources/js/components/Info/SetsAndLegsTable.vue
+++ resources/js/components/Info/SetsAndLegsTable.vue
@@ -1,26 +1,24 @@
 <template>
-	<div class="w-full text-white">
-		<div class="flex flex-col text-lg">
-			<div class="flex px-3 py-1">
-				<div class="w-7/12"></div>
-				<div class="w-3/12">Sets</div>
-				<div class="w-2/12">Legs</div>
+	<div class="w-full h-full text-white flex flex-col text-lg justify-end">
+		<div class="flex px-3 py-1">
+			<div class="w-7/12"></div>
+			<div class="w-3/12">Sets</div>
+			<div class="w-2/12">Legs</div>
+		</div>
+		<div
+			v-for="user in users"
+			:key="user.id"
+			class="flex bg-white text-blue-500 font-semibold rounded-lg px-3 py-3 mb-2"
+		>
+			<div class="w-7/12">
+				{{user.name}}
+				<div
+					v-show="user.owns_current_leg"
+					class="inline-flex rounded-full h-2 w-2 bg-blue-500"
+				></div>
 			</div>
-			<div
-				v-for="user in users"
-				:key="user.id"
-				class="flex bg-white text-blue-500 font-semibold rounded-lg px-3 py-3 mb-2"
-			>
-				<div class="w-7/12">
-					{{user.name}}
-					<div
-						v-show="user.owns_current_leg"
-						class="inline-flex rounded-full h-2 w-2 bg-blue-500"
-					></div>
-				</div>
-				<div class="w-3/12">{{user.sets_won}}</div>
-				<div class="w-2/12">{{user.legs_won}}</div>
-			</div>
+			<div class="w-3/12">{{user.sets_won}}</div>
+			<div class="w-2/12">{{user.legs_won}}</div>
 		</div>
 	</div>
 </template>
