e30e72724073cdc24c2f0b0c5c3ed934c1795016
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-03-31 16:55:00 +0200

    sync users to game

--- app/Http/Controllers/SaveGameController.php
+++ app/Http/Controllers/SaveGameController.php
@@ -10,8 +10,13 @@ class SaveGameController extends Controller
 	public function game(Request $request)
 	{
 		$game = collect($request->game);
+		$users = collect($request->users);
+		$users_only_ids = $users->map(function ($user) {
+			return $user->id;
+		});
 
 		$game = Game::create($game);
-		
+		$game->users()->sync($users_only_ids);
+
 	}
 }
