1da769371e7af5c6a19a71292c0d8cb895caf881
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-04-28 21:55:52 +0200

    graph scrollable

--- resources/js/components/Graph/GraphTurns.vue
+++ resources/js/components/Graph/GraphTurns.vue
@@ -1,73 +1,77 @@
 <template>
-  <div class="rounded-lg flex justify-center items-center h-full relative">
-    <svg
-      v-for="user in users"
-      :key="user.id"
-      xmlns="http://www.w3.org/2000/svg"
-      ref="graph"
-      :viewBox="`-2 -4 ${width + 4} ${height + 4}`"
-      width="100%"
-      height="100%"
-      preserveAspectRatio="none"
-      class="absolute"
-      :class="user.is_on_turn ? 'stroke-white z-20' : 'stroke-lightblue z-10'"
-    >
-      <path
-        :ref="`graph_line_${user.id}`"
-        :id="`graph_line_${user.id}`"
-        :d="getLinePath(user)"
-        fill="none"
-        stroke-width="3"
-        :pathLength="user.turns.length"
-        :stroke-linecap="user.turns.length >= 1 ? 'round' : ''"
-        style="transition: stroke 0.3s ease-in-out"
-      />
-    </svg>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      :viewBox="`-2 -4 ${width + 4} ${height + 4}`"
-      width="100%"
-      height="100%"
-      preserveAspectRatio="none"
-      class="absolute z-0"
-    >
-      <line
-        v-for="(point, i) in graph_points + 1"
-        :key="point"
-        :x1="i * scale_x"
-        :y1="0"
-        :x2="i * scale_x"
-        :y2="height"
-        stroke-width="2.5px"
-        class="stroke-graphaxis opacity-10"
-      ></line>
-    </svg>
-    <div class="h-full w-full absolute flex z-50">
-      <transition name="tab-fade" mode="out-in">
-        <div
-          v-if="graphTurnData.length > 0"
-          :key="`graph_index_${currentGraphIndex}`"
-          class="p-2 right-0 absolute grid gap-y-1"
-        >
-          <div
-            v-for="turn in graphTurnData"
-            :key="`graph_turn_${turn.user_id}`"
-            class="rounded leading-4 px-1 text-right bg-blue-100 text-blue-900 font-semibold"
-          >
-            {{ turn.thrown_score }}
-          </div>
-        </div>
-      </transition>
-      <div
-        v-for="(point, i) in graph_points"
-        :key="'bar_' + point"
-        class="h-full opacity-20 rounded transition-colors"
-		:class="currentGraphIndex == i ? 'bg-blue-100' : ''"
-        :style="{ width: `calc(100% / ${graph_points})` }"
-        @click="getTurnsAtIndex(i)"
-      ></div>
-    </div>
-  </div>
+
+	<div
+		ref="graph"
+		class="flex justify-center items-center h-full overflow-scroll relative"
+	>
+		<svg
+			v-for="user in users"
+			:key="user.id"
+			xmlns="http://www.w3.org/2000/svg"
+			height="100%"
+			:width="`${graph_points * scale_x}px`"
+			:viewBox="`-2 -4 ${graph_points * scale_x} ${height + 4}`"
+			preserveAspectRatio="none"
+			class="absolute top-0 left-0"
+			:class="user.is_on_turn ? 'stroke-white z-20' : 'stroke-lightblue z-10'"
+		>
+			<path
+				:ref="`graph_line_${user.id}`"
+				:id="`graph_line_${user.id}`"
+				:d="getLinePath(user)"
+				fill="none"
+				stroke-width="3"
+				:pathLength="user.turns.length"
+				:stroke-linecap="user.turns.length >= 1 ? 'round' : ''"
+				style="transition: stroke 0.3s ease-in-out"
+			/>
+		</svg>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			:width="`${graph_points * scale_x}px`"
+			:viewBox="`-2 -4 ${graph_points * scale_x} ${height + 4}`"
+			height="100%"
+			preserveAspectRatio="none"
+			class="absolute z-0 top-0 left-0"
+		>
+			<line
+				v-for="(point, i) in graph_points + 1"
+				:key="point"
+				:x1="i * scale_x"
+				:y1="0"
+				:x2="i * scale_x"
+				:y2="height"
+				stroke-width="2.5px"
+				class="stroke-graphaxis opacity-10"
+			></line>
+		</svg>
+		<transition
+			name="tab-fade"
+			mode="out-in"
+		>
+			<div
+				v-if="graphTurnData.length > 0"
+				:key="`graph_index_${currentGraphIndex}`"
+				class="p-2 right-0 top-0 absolute grid gap-y-1"
+			>
+				<div
+					v-for="turn in graphTurnData"
+					:key="`graph_turn_${turn.user_id}`"
+					class="rounded leading-4 px-1 text-right bg-blue-100 text-blue-900 font-semibold"
+				>
+					{{ turn.thrown_score }}
+				</div>
+			</div>
+		</transition>
+		<div
+			v-for="(point, i) in graph_points"
+			:key="'bar_' + point"
+			class="h-full z-50 absolute opacity-20 transition-colors"
+			:class="currentGraphIndex == i ? 'bg-blue-300' : ''"
+			:style="{left: `${i * scale_x}px`, width: `${(1 * scale_x) + 2}px`}"
+			@click="getTurnsAtIndex(i)"
+		></div>
+	</div>
 </template>
 
 <script>
@@ -76,105 +80,109 @@ import { line, curveMonotoneX } from "d3-shape";
 import anime from "animejs";
 
 export default {
-  name: "graph-turns",
-  data() {
-    return {
-      width: null,
-      height: null,
-      scale_y: null,
-      scale_x: null,
-      graph_points: 5, //default
-      paths: [],
-      currentGraphIndex: undefined,
-      graphTurnData: [],
-    };
-  },
-  mounted() {
-    this.updateScale();
-    window.addEventListener("resize", () => {
-      this.updateScale();
-    });
-  },
-  computed: {
-    ...mapState({
-      users: (state) => state.users,
-      startScore: (state) => state.start_score,
-      user_on_turn: (state) => state.users.find((u) => u.is_on_turn),
-    }),
-  },
-  methods: {
-    updateScale() {
-      if (this.$refs.graph == undefined) return;
+	name: "graph-turns",
+	data() {
+		return {
+			width: null,
+			height: null,
+			scale_y: null,
+			scale_x: null,
+			graph_points: 4, //default
+			paths: [],
+			currentGraphIndex: undefined,
+			graphTurnData: [],
+		};
+	},
+	mounted() {
+		this.updateScale();
+		window.addEventListener("resize", () => {
+			this.updateScale();
+		});
+	},
+	computed: {
+		...mapState({
+			users: (state) => state.users,
+			startScore: (state) => state.start_score,
+			user_on_turn: (state) => state.users.find((u) => u.is_on_turn),
+		}),
+	},
+	methods: {
+		updateScale() {
+			if (this.$refs.graph == undefined) return;
 
-      if (this.user_on_turn.turns.length == 0) this.graph_points = 5;
+			this.incrementGraphPoints();
 
-      if (this.user_on_turn.turns.length >= this.graph_points + 1)
-        this.graph_points += 1;
+			this.width = this.$refs.graph.clientWidth;
+			this.height = this.$refs.graph.clientHeight;
+			this.scale_x = this.width / 5;
+			this.scale_y = this.height / this.startScore;
+		},
 
-      this.width = this.$refs.graph.clientWidth;
-      this.height = this.$refs.graph.clientHeight;
-      this.scale_x = this.width / this.graph_points;
-      this.scale_y = this.height / this.startScore;
-    },
+		incrementGraphPoints() {
+			if (this.user_on_turn.turns.length == 0) this.graph_points = 5;
 
-    getPointsFromTurns(turns) {
-      this.updateScale();
-      let points = turns.map((turn, i) => [
-        (i + 1) * this.scale_x,
-        this.height - turn.new_score_to_throw_from * this.scale_y,
-        turn.thrown_score,
-      ]);
+			if (this.user_on_turn.turns.length >= this.graph_points + 1)
+				this.graph_points += 1;
+		},
 
-      //push the startpoint at the beginning of the array
-      points.unshift([0, this.height - this.startScore * this.scale_y]);
-      return points;
-    },
+		getPointsFromTurns(turns) {
+			this.incrementGraphPoints();
+			let points = turns.map((turn, i) => [
+				(i + 1) * this.scale_x,
+				this.height - turn.new_score_to_throw_from * this.scale_y,
+				turn.thrown_score,
+			]);
 
-    getLinePath(user) {
-      let element = this.$refs[`graph_line_${user.id}`];
+			//push the startpoint at the beginning of the array
+			points.unshift([0, this.height - this.startScore * this.scale_y]);
+			return points;
+		},
 
-      let points = this.getPointsFromTurns(user.turns);
+		getLinePath(user) {
+			let element = this.$refs[`graph_line_${user.id}`];
 
-      this.animatePath(element, user);
+			let points = this.getPointsFromTurns(user.turns);
 
-      let lineGenerator = line().curve(curveMonotoneX);
-      return lineGenerator(points);
-    },
+			this.animatePath(element, user);
 
-    getTurnsAtIndex(index) {
-      if (this.graphTurnData.length > 0 && this.currentGraphIndex == index) {
-        this.graphTurnData = [];
-		this.currentGraphIndex = undefined;
-        return;
-      }
+			let lineGenerator = line().curve(curveMonotoneX);
+			return lineGenerator(points);
+		},
 
-      this.currentGraphIndex = index;
+		getTurnsAtIndex(index) {
+			if (this.graphTurnData.length > 0 && this.currentGraphIndex == index) {
+				this.graphTurnData = [];
+				this.currentGraphIndex = undefined;
+				return;
+			}
 
-      this.graphTurnData = this.users.map((user) => {
-        return user.turns[index];
-      });
-    },
+			this.currentGraphIndex = index;
 
-    animatePath(path_element, user) {
-      if (!path_element) return;
-      if (!user.is_on_turn) return;
+			this.graphTurnData = this.users.map((user) => {
+				return user.turns[index];
+			});
+		},
 
-      if (path_element.style.strokeDasharray > user.turns.length)
-        path_element.style.strokeDasharray = 0;
+		animatePath(path_element, user) {
+			if (!path_element) return;
+			if (!user.is_on_turn) return;
 
-      //animatie the svg path only when it has added a new point
-      if (path_element.style.strokeDasharray < user.turns.length) {
-        path_element.style.strokeDasharray = user.turns.length;
-        path_element.style.strokeDashoffset = 1;
+			if (path_element.style.strokeDasharray > user.turns.length)
+				path_element.style.strokeDasharray = 0;
 
-        anime({
-          targets: path_element,
-          strokeDashoffset: 0,
-          duration: 250,
-          easing: "easeOutQuad",
-        });
-      }
-    },
-  },
+			//animatie the svg path only when it has added a new point
+			if (path_element.style.strokeDasharray < user.turns.length) {
+				path_element.style.strokeDasharray = user.turns.length;
+				path_element.style.strokeDashoffset = 1;
+
+				anime({
+					targets: path_element,
+					strokeDashoffset: 0,
+					duration: 250,
+					easing: "easeOutQuad",
+				});
+			}
+		},
+	},
 };
 </script>
--- resources/js/components/Tabs/Tabs.vue
+++ resources/js/components/Tabs/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="w-full flex-100 min-h-32 flex my-2">
 		
-		<div class="p-1 px-2 w-10/12 flex-1 bg-blue-500 rounded-b-lg rounded-tl-lg">
+		<div class="p-2 w-10/12 flex-1 bg-blue-500 rounded-b-lg rounded-tl-lg">
 			<transition name="tab-fade" mode="out-in">
 			<keep-alive>
 				<graph-turns v-if="selected_index == 0" />
