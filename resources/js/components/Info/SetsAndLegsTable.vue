6b2b804d9431b9b4e349c9b6a6bf73db0742a5dc
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-04-30 16:37:15 +0200

    different table layout

--- resources/js/components/Info/SetsAndLegsTable.vue
+++ resources/js/components/Info/SetsAndLegsTable.vue
@@ -1,37 +1,36 @@
 <template>
-  <div class="text-white flex flex-col text-lg h-full overflow-scroll relative">
+  <div class="text-white flex flex-col h-full text-lg rounded-lg overflow-scroll relative first:rounded-t-lg">
     <div
-      class="w-full z-0 flex gap-x-1 px-3 text-sm xsm:text-base"
+      class="w-full z-0 py-0.5 flex text-sm xsm:text-base bg-blue-100 bg-opacity-50"
     >
       <div class="w-4/12"></div>
       <div
         v-for="user in users"
         :key="user.id"
-        class="w-4/12 flex justify-start px-1 items-baseline bg-blue-100 bg-opacity-50 rounded-t"
+        class="w-4/12 flex justify-between px-1 items-baseline"
       >
         <div class="overflow-hidden whitespace-nowrap overflow-ellipsis"
-        style="width:85%;">
+        style="max-width:85%;">
           {{ user.name }}
         </div>
         <div
           v-if="user.owns_current_leg"
-          class="rounded-full ml-1 h-1.5 w-1.5"
-          :class="user.is_on_turn ? 'bg-white' : 'bg-blue-500'"
+          class="rounded-full ml-1 h-1.5 w-1.5 bg-white"
         ></div>
       </div>
     </div>
       <div
         v-for="stat in stats"
         :key="`stats_${stat.name}`"
-        class="flex bg-white text-blue-500 font-semibold rounded-lg shadow px-2 py-1 mb-1 text-base xsm:text-lg"
+        class="last:rounded-b-lg flex bg-white text-blue-500 font-semibold shadow px-2 py-1 text-base xsm:text-lg"
       >
         <div class="w-4/12 flex items-center">
           {{ stat.name }}
         </div>
-        <div class="w-4/12 justify-center flex items-center">
+        <div class="w-4/12 justify-end flex items-center">
           {{ users[0].stats[stat.key] }}
         </div>
-        <div class="w-4/12 justify-center flex items-center">
+        <div class="w-4/12 justify-end flex items-center">
           {{ users[1].stats[stat.key] }}
         </div>
       </div>
