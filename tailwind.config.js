968279c10d2234eefb1d86d921d66ce91cf790d8
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-05-04 11:35:24 +0200

    custom shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,9 @@ module.exports = {
             screens: {
                 'xsm': '375px',
                 'xs': '425px'
+            },
+            boxShadow: {
+                'up': '0px 10px 15px 5px rgba(0, 0, 0, 0.1), 0 4px 6px 6px rgba(0, 0, 0, 0.05)'
             }
 		},
         stroke: theme => ({
