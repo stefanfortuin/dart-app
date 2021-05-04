398e570c00e7276b50501da4067960d021a0892f
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-05-04 19:45:52 +0200

    font bold on active nav link

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,11 +1,11 @@
 <a href="{{$href}}"
     class="flex flex-col justify-center items-center active:bg-blue-100
-        {{ '/'.Request::segment(1) == $href ? 'text-blue-500' : 'text-gray-500'}}
+        {{ '/'.Request::segment(1) == $href ? 'text-blue-500 font-bold' : 'text-gray-500 font-light'}}
     ">
     <svg class="icon fill-current">
         <use href="/assets/sprite.svg#{{$icon}}"></use>
     </svg>
-    <div class="font-light text-xs">
+    <div class="text-xs">
         {{ $title }}
     </div>
 </a>
