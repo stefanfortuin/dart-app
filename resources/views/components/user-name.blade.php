0b3c910b7b2fb68288276ed88ea29f70e50c11a5
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-05-04 14:44:59 +0200

    blade user name component

---
+++ resources/views/components/user-name.blade.php
@@ -0,0 +1,3 @@
+<div class="text-xl overflow-hidden whitespace-nowrap overflow-ellipsis" style="max-width: 90%">
+    {{ $name }}
+</div>
--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -1,13 +1,9 @@
 <x-base>
-	<h1 class="font-bold text-4xl text-gray-700 mb-3">Statistieken</h1>
-	<div class="flex flex-col my-4">
+	<div class="flex flex-col">
 		@foreach ($users as $user)
 			<a href="{{'/stats/'.$user->id}}" class="text-2xl font-bold text-white my-1 px-4 py-3 bg-blue-500 rounded-lg">
-				{{$user->name}}
+				<x-user-name name="{{$user->name}}" />
 			</a>
 		@endforeach
-	</div>
-	<div class="mt-auto">
-		<x-button-action href="/">Terug</x-button-action>
 	</div>
 </x-base>
