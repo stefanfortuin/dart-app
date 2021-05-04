d3068c1124b1ed66ea4c6caf609719467a606211
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-05-04 09:33:13 +0200

    input field components

---
+++ resources/js/components/InputNumberField.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="flex flex-col mb-2">
+		<label :for="id" class="mb-1 text-xl font-semibold text-blue-400">{{label}}</label>
+		<input
+			type="number"
+			:value="modelValue"
+			:id="id"
+			:min="min"
+			:max="max"
+			@input="$emit('update:modelValue', $event.target.value)"
+			class="border-2 border-blue-200 py-2 px-3 placeholder-blue-100 rounded-lg text-3xl font-bold text-blue-500"
+		>
+	</div>
+</template>
+
+<script>
+export default {
+	props: [
+		'modelValue',
+		'id',
+		'label',
+		'max',
+		'min'
+	]
+}
+</script>
--- resources/js/views/GameSettings.vue
+++ resources/js/views/GameSettings.vue
@@ -1,137 +1,115 @@
 <template>
-	<div class="h-full w-full flex flex-col">
-		<div>
-			<PlayerInput
-				id="player_one"
-				label="Speler 1"
-				v-model="data.user_one"
-			/>
-			<PlayerInput
-				id="player_two"
-				label="Speler 2"
-				v-model="data.user_two"
-			/>
-			<div class="flex flex-col w-full mb-2">
-				<label
-					for="set_start_score"
-					class="mb-1 text-xl font-semibold text-blue-400"
-				>Start score</label>
-				<input
-					type="number"
-					min="101"
-					max="501"
-					name="set_start_score"
-					id="set_start_score"
-					class="border-2 border-blue-200 text-blue-500 font-bold text-3xl py-2 px-3 rounded-lg"
-					v-model="start_score"
-				/>
-			</div>
-			<div class="grid grid-cols-2 gap-2 w-full mb-2">
-				<div class="flex flex-col">
-					<label
-						for="set_total_sets"
-						class="mb-1 text-xl font-semibold text-blue-400"
-					>Sets</label>
-					<input
-						type="number"
-						min="1"
-						name="set_total_sets"
-						id="set_total_sets"
-						class="border-2 border-blue-200 text-blue-500 font-bold text-3xl py-2 px-3 rounded-lg"
-						v-model="total_sets"
-					/>
-				</div>
-				<div class="flex flex-col">
-					<label
-						for="set_total_legs"
-						class="mb-1 text-xl font-semibold text-blue-400"
-					>Legs/set</label>
-					<input
-						type="number"
-						min="1"
-						name="set_total_legs"
-						id="set_total_legs"
-						class="border-2 border-blue-200 text-blue-500 font-bold text-3xl py-2 px-3 rounded-lg"
-						v-model="total_legs"
-					/>
-				</div>
-			</div>
-		</div>
+  <div class="h-full w-full flex flex-col">
+    <div>
+      <input-text-field
+        id="player_one"
+        label="Speler 1"
+        v-model="data.user_one"
+      />
+      <input-text-field
+        id="player_two"
+        label="Speler 2"
+        v-model="data.user_two"
+      />
+      <div class="flex flex-col w-full mb-2">
+        <input-number-field
+          label="Start score"
+          v-model="start_score"
+          max="501"
+          min="101"
+        />
+      </div>
+      <div class="grid grid-cols-2 gap-2 w-full mb-2">
+        <div class="flex flex-col">
+          <input-number-field label="Sets" v-model="total_sets" min="1" />
+        </div>
+        <div class="flex flex-col">
+          <input-number-field label="Legs/set" v-model="total_legs" min="1" />
+        </div>
+      </div>
+    </div>
 
-		<div class="flex flex-col w-full mt-auto">
-			<button-action
-				@click="handleNextStep()"
-				:disabled="!canProceed"
-			>Start spel</button-action>
+    <div class="flex flex-col w-full mt-auto">
+      <button-action @click="handleNextStep()" :disabled="!canProceed"
+        >Start spel</button-action
+      >
 
-			<button-action href="/stats">Statistieken</button-action>
-		</div>
-	</div>
+      <button-action href="/stats">Statistieken</button-action>
+    </div>
+  </div>
 </template>
 
 <script>
-import PlayerInput from "../components/PlayerInput";
+import InputTextField from "../components/InputTextField.vue";
+import InputNumberField from "../components/InputNumberField.vue";
 import { mapMutations } from "vuex";
 import ButtonAction from "../components/ButtonAction.vue";
 
 export default {
-	data() {
-		return {
-			data: {
-				user_one: "",
-				user_two: "",
-			},
-			start_score: 501,
-			total_sets: 1,
-			total_legs: 1,
-		};
-	},
-	components: {
-		PlayerInput,
-		ButtonAction,
-	},
-	computed: {
-		canProceed() {
-			return this.fieldsAreNotEmpty && this.usersAreNotTheSame;
-		},
+  data() {
+    return {
+      data: {
+        user_one: "",
+        user_two: "",
+      },
+      start_score: 501,
+      total_sets: 1,
+      total_legs: 1,
+    };
+  },
+  components: {
+    InputTextField,
+    InputNumberField,
+    ButtonAction,
+  },
+  computed: {
+    canProceed() {
+      return this.fieldsAreNotEmpty && this.usersAreNotTheSame;
+    },
 
-		usersAreNotTheSame() {
-			return (
-				this.data.user_one.toLowerCase() != this.data.user_two.toLowerCase()
-			);
-		},
+    usersAreNotTheSame() {
+      return (
+        this.data.user_one.toLowerCase() != this.data.user_two.toLowerCase()
+      );
+    },
 
-		fieldsAreNotEmpty() {
-			return (
-				this.data.user_one != "" &&
-				this.data.user_two != "" &&
-				this.start_score != ""
-			);
-		},
-	},
-	methods: {
-		...mapMutations(["setUsers", "goToNextStep", "setStartScore", "setTotalSets", "setTotalLegs"]),
+    fieldsAreNotEmpty() {
+      return (
+        this.data.user_one != "" &&
+        this.data.user_two != "" &&
+        this.start_score != ""
+      );
+    },
+  },
+  methods: {
+    ...mapMutations([
+      "setUsers",
+      "goToNextStep",
+      "setStartScore",
+      "setTotalSets",
+      "setTotalLegs",
+    ]),
 
-		handleNextStep() {
-			if (!this.canProceed) return;
+    handleNextStep() {
+      if (!this.canProceed) return;
 
-			this.setStartScore(this.start_score);
-			this.setTotalSets(this.total_sets);
-			this.setTotalLegs(this.total_legs);
+      this.setStartScore(this.start_score);
+      this.setTotalSets(this.total_sets);
+      this.setTotalLegs(this.total_legs);
 
-			fetch(`/api/users`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(this.data),
-			})
-				.then((response) => response.json())
-				.then((response) => {
-					this.setUsers(response);
-					this.goToNextStep();
-				});
-		},
-	},
+      fetch(`/api/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.data),
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          this.setUsers(response);
+          this.goToNextStep();
+        });
+    },
+  },
 };
 </script>
