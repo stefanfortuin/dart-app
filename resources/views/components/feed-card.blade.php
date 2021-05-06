e84498447552c1fb4f190c1cd887aeae18610ac0
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-05-06 16:58:23 +0200

    more feed changes

--- resources/views/components/feed-card.blade.php
+++ resources/views/components/feed-card.blade.php
@@ -3,11 +3,11 @@ $user_two = count($game->users) > 1 ? $game->users[1] : null;
 $won = Auth::id() == $game->winner_id;
 @endphp
 
-<div class="py-2 px-3 rounded-lg border-2 border-gray-200">
+<div class="py-2 px-3 rounded-lg border-2 border-blue-200">
     <div class="flex flex-col items-baseline justify-between leading-5 mb-2">
         <div class="font-light overflow-hidden whitespace-nowrap overflow-ellipsis" style="max-width: 90%;">
             <span class="font-semibold">{{$game->users[0]->name}}</span>
-            <span class="{{$won ? 'text-green-600' : 'text-red-400'}}">{{ $won ? 'won' : 'verloor'}}</span> van
+            {{ $won ? 'won' : 'verloor'}} van
             {{($user_two != null) ? $user_two->name : 'gast'}}
         </div>
         <div class="text-xs font-light text-opacity-50">{{$game->played_at}}</div>
