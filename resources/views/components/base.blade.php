69cde105f0ea5dc07d5efddbd59857322cf7c94f
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-05-07 10:38:29 +0200

    fonts

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -5,8 +5,12 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="{{mix('/css/app.css')}}">
 	<title>Darts</title>
+	<link rel="preconnect" type="font/woff2" as="font" href="/assets/fonts/poppins-v15-latin-regular.woff2">
+	<link rel="preconnect" type="font/woff2" as="font" href="/assets/fonts/poppins-v15-latin-600.woff2">
+	<link rel="preconnect" type="font/woff2" as="font" href="/assets/fonts/poppins-v15-latin-700.woff2">
+	<link rel="stylesheet" href="{{mix('/css/app.css')}}">
+	<link href="/assets/sprite.svg" as="image" type="image/svg+xml"/>
 </head>
 
 <body class="antialiased max-w-md mx-auto p-3 flex flex-col">
