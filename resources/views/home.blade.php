9098b80c14774d66a85d29933c458da15290329f
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-05-21 15:29:51 +0200

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,13 @@
 <x-base>
-    Dit is de home pagina
+    {{-- hero --}}
+    <div class="flex flex-col justify-between">
+        <h1 class="text-6xl font-bold text-blue-500">Let's play darts!</h1>
+        <div class="text-xl mt-2 mb-4">
+            Houd je dartscore nu nog gemakkelijker bij!
+        </div>
+        <a href="/play"
+            class=" flex items-center max-w-max font-semibold text-xl text-white py-2 px-4 rounded-lg shadow my-2 bg-blue-500 select-none">
+            Speel nu
+        </a>
+    </div>
 </x-base>
