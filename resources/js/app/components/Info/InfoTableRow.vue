ca8299a5eb761b517c52ac3cf6e55ddb2e443a08
Stefan Fortuin <stefanfortuin02@gmail.com>
2021-05-29 12:52:30 +0200

    info table grid fix

--- resources/js/app/components/Info/InfoTableRow.vue
+++ resources/js/app/components/Info/InfoTableRow.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="w-full">
-    <div class="flex justify-between items-center mb-1">
-      <div class="font-bold">{{ leg.stats[users[0].uuid][stat.key].toFixed(stat.decimal) }}</div>
-      <div class="font-semibold text-blue-500">{{ stat.name }}</div>
-      <div class="font-bold">{{ leg.stats[users[1].uuid][stat.key].toFixed(stat.decimal) }}</div>
+    <div class="grid grid-cols-3 mb-1">
+      <div class="font-bold text-left">{{ leg.stats[users[0].uuid][stat.key].toFixed(stat.decimal) }}</div>
+      <div class="font-semibold text-blue-500 text-center">{{ stat.name }}</div>
+      <div class="font-bold text-right">{{ leg.stats[users[1].uuid][stat.key].toFixed(stat.decimal) }}</div>
     </div>
     <div class="grid grid-cols-2 gap-3">
       <progress-bar
